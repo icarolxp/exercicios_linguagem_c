1759e2bc3f4af8db525892abc40b3a46a4d25d0b
icarolxp <icaro2304lucas@gmail.com>
2025-08-27 15:11:50 -0400

    Add files via upload

---
+++ lista_sequencial/quest36.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float custoEspetaculo, precoConvite;
+    int convitesParaCusto, convitesParaLucro;
+
+    printf("Digite o custo do espetaculo teatral: ");
+    scanf("%f", &custoEspetaculo);
+    printf("Digite o preco do convite: ");
+    scanf("%f", &precoConvite);
+
+    // Quantidade para cobrir o custo (arredondando para cima)
+    convitesParaCusto = (int)(custoEspetaculo / precoConvite) + 1;
+
+    // Quantidade para ter 23% de lucro
+    convitesParaLucro = (int)((custoEspetaculo * 1.23) / precoConvite) + 1;
+
+    printf("\nQuantidade de convites para alcancar o custo: %d\n", convitesParaCusto);
+    printf("Quantidade de convites para ter 23%% de lucro: %d\n", convitesParaLucro);
+
+    return 0;
+}
